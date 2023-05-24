581f671ebed5becd40e6cf4bebf54eefa9adbb01
jfmarkan <jfmarkan@gmail.com>
2023-05-24 11:00:22 +0200

    corrected code after class

--- script.js
+++ script.js
@@ -1,48 +1,49 @@
 const playButton = document.querySelector('button');
-const gridElement = document.getElementById('grid');
+
 
 
 
 playButton.addEventListener('click', function(){
     grid.innerHTML = '';
     console.clear();
-    let dificultySelector = document.getElementById('dificulty');
-    if (dificultySelector.value == 1){
-        for (let i = 1; i<=100 ; i++) {
-            const newCell = createElement('div', 'my_cell-one', i);
-                newCell.addEventListener('click', function(){
-                    newCell.classList.toggle('my_selected');
-                    console.log(i)
-                });
-            gridElement.appendChild(newCell);
-        }
-    } else if (dificultySelector.value == 2){
-        for (let i = 1; i<=81 ; i++) {
-            const newCell = createElement('div', 'my_cell-two', i);
-                newCell.addEventListener('click', function(){
-                    newCell.classList.toggle('my_selected');
-                    console.log(i)
-                });
-            gridElement.appendChild(newCell);
-        }
-    } else if (dificultySelector.value == 3){
-        for (let i = 1; i<=49 ; i++) {
-            const newCell = createElement('div', 'my_cell-three', i);
-                newCell.addEventListener('click', function(){
-                    newCell.classList.toggle('my_selected');
-                    console.log(i)
-                });
-            gridElement.appendChild(newCell);
-        }
-    }
-    
+    startNewGame();
 });
+    
+    
+function startNewGame(){
+    const gridElement = document.getElementById('grid');
+    const dificultySelector = document.getElementById('dificulty').value;
+
+    let cellsNumber = 0;
+    let cellsClass = "";
+
+    if (dificultySelector == 1){
+        cellsNumber = 100;
+        cellsClass = "my_cell-one"
+    } else if (dificultySelector == 2) {
+        cellsNumber = 81;
+        cellsClass = "my_cell-two"
+    } else if (dificultySelector == 3){
+        cellsNumber = 49;
+        cellsClass = "my_cell-three"
+    }
+
+    for (let i = 1; i<=cellsNumber ; i++) {
+        const newCell = createElement('div', cellsClass, i);
+            newCell.addEventListener('click', function(){
+                newCell.classList.toggle('my_selected');
+                console.log(i)
+            });
+        gridElement.appendChild(newCell);
+    }    
+};
 
 /**
  * Function that creates a custom HTML element with the given tag and classes (as a string)
  *
- * @param {string} tagName The tag of the element to be created as a string
- * @param {string} className The classes of the element to be created as a string
+ * @param {string} tagName The tag of the element to be created as a string.
+ * @param {string} className The classes of the element to be created as a string.
+ * @param {string} innerText Content inside the div cell.
  */
 function createElement(tagName, className, innerText){
     const cellElement = document.createElement(tagName);
